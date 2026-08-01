d5954f7496365b7cdd6ad77a3232f98eb92c3a50
tannanarasimhadheeraj25csm-glitch <tannanarasimhadheeraj.25.csm@anits.edu.in>
2026-08-01 23:20:06 +0530

    Add files via upload

---
+++ methods.java
@@ -0,0 +1,26 @@
+package core;
+public class methods{
+    public void inputs(){
+        System.out.println("hello!!");
+    }
+    public void display(int x,int y){
+        System.out.println(x+y);
+    }
+    public int sample(int x,int y){
+        return x+y;
+    }
+    public int output(){
+        int x=20,y=10;
+        return x+y;
+    }
+    public static void main(String[] args){
+         methods d=new methods();
+         d.inputs();
+         d.display(10,20);
+            int sum=d.sample(10,20);
+            System.out.println(sum);
+            int result=d.output();
+            System.out.println(result);
+    }
+}
+
